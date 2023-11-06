e49f271cc9ec303235aa5d302c56b2b85b24e783
jagpratap <jagpratapchauhan@gmail.com>
2023-11-06 17:53:01 +0530

    Connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
