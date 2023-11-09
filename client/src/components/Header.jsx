2649fa96000229f866c91b73ae55df0ab43738d9
jagpratap <jagpratapchauhan@gmail.com>
2023-11-10 00:49:54 +0530

    Update Profile page to private route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,9 +32,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className=" text-slate-700 hover:underline"> Sign in</li>
-          </Link>
+          {currentUser ? (
+            <Link to="/profile">
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            </Link>
+          ) : (
+            <Link to="/sign-in">
+              <li className=" text-slate-700 hover:underline">Sign in</li>
+            </Link>
+          )}
         </ul>
       </div>
     </header>
